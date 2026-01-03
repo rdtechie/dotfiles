a155c5a9da192aa5063936b6151e6fa0532ee799
rdtechie <rdtechie@users.noreply.github.com>
2026-01-03 11:30:37 +0100

    chore(nvim): update snacks

---
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,24 @@
+return {
+	{
+		"folke/snacks.nvim",
+		---@type snacks.Config
+		opts = {
+			explorer = {
+				-- your explorer configuration comes here
+				-- or leave it empty to use the default settings
+				-- refer to the configuration section below
+			},
+			picker = {
+				sources = {
+					explorer = {
+						hidden = true,
+						ignored = true,
+						layout = { layout = { position = "right" } },
+						-- your explorer picker configuration comes here
+						-- or leave it empty to use the default settings
+					},
+				},
+			},
+		},
+	},
+}
